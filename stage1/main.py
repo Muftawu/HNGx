798c811a897381c83ecec2f60cd68c80db81f8d3
Muftawu <mohammedyiwere@gmail.com>
2023-09-09 14:50:03 +0000

    changed server to waitress

--- stage1/main.py
+++ stage1/main.py
@@ -26,6 +26,6 @@ def get_details():
     return jsonify(DATA)
 
 if __name__ == '__main__':
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run(debug=True)
