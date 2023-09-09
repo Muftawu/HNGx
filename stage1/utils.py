b39fb990498febecd241b3d7aaf81e4623236eb3
Muftawu <mohammedyiwere@gmail.com>
2023-09-09 15:33:38 +0000

    fixed utc time format

--- stage1/utils.py
+++ stage1/utils.py
@@ -2,6 +2,8 @@ import calendar
 import datetime 
 from datetime import date
 
+# recommended time : 2023-09-09T15:24:06Z
+
 d = date.today()
 
 def get_date_today():
@@ -11,6 +13,7 @@ def get_day_of_week():
     return calendar.day_name[d.weekday()]
 
 def get_utc_time():
-    return datetime.datetime.now()
+    t = str(datetime.datetime.now()).split('.')[0]
+    return t[:10] + 'T' + t[11:] + 'Z'
 
 print(get_utc_time())
