7483cb9b065bc33f6f2e0bf453b766036b43b02a
Muftawu <mohammedyiwere@gmail.com>
2023-09-14 15:01:15 +0000

    added static folder

--- stage2/hngx_proj/hngx_proj/settings.py
+++ stage2/hngx_proj/hngx_proj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = False 
 
 ALLOWED_HOSTS = ['*']
 
@@ -149,6 +149,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
